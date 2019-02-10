8b7dc43141b34d5108dba53596774b8b54b46ae3
MG <michal.gulczynski93@gmail.com>
2019-02-10 14:27:37 +0100

    before moving the enemy 137

---
+++ game/Scenes/Enemy.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	queue_free()
+
+
+func _on_GunTimer_timeout():
+	pass # replace with function body
